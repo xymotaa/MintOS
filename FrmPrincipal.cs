6681d060fd2fc419ef1d006abc2ceace79f65a83
Lucas Mota <lucas.barros@unifesspa.edu.br>
2026-08-25 02:01:49 -0300

    adding a close function to the exit button, adding the registration screen function, and editing the readme

--- FrmPrincipal.cs
+++ FrmPrincipal.cs
@@ -19,14 +19,20 @@ namespace MintOs
 
         private void MenuSair_Click(object sender, EventArgs e)
         {
+            // this em C# é chamado de "referência implícita" e se refere à instância atual da classe.
+            // close() é um método que fecha a janela atual, liberando todos os recursos associados a ela.
             this.Close();
         }
 
         private void MenuCadastroFuncionários_Click(object sender, EventArgs e)
         {
             // Depois do duplo clique, primeiro se chama a pasta
+            // Depois do duplo clique, se chaama o formulário de cadastro
+            // Depois de chamar o formulário, cria uma estância do formulário e exibe ele como uma janela modal
+            // O ShowDialog() exibe o formulário como uma janela modal, ou seja, o usuário não pode interagir com a janela principal até que a janela modal seja fechada.
             cadastro.FrmCadastro Frm = new cadastro.FrmCadastro();
             Frm.ShowDialog();
         }
     }
 }
+
---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">MintOS</h1>
+
+<p align="justify">
+    MintOS é um sistema PDV focado em suprir as necessidades de empresas que não possuem capital para pagar por sistemas complexos ou de alto custo mensal.
+</p>
+
+<p align="left">
+    Com isso, surge o MintOS. Aguarde novas atualizações!
+</p>
+
+<p align="left">
+    <img src="http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge"/>
+</p>
